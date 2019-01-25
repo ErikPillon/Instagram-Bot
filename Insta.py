2e4af0d15afc2e91eef0a81f23411290f28368db
Erik Pillon <erik.pillon@gmail.com>
2019-01-25 15:49:34 +0100

    Added the Script. Added gitignore & created the folder for the personal data

---
+++ Insta.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time #essentially only for the sleep() feature
+import os
+# the following is deprecated and not needed
+# from selenium.webdriver.common.by import By 
+
+if (os.path.isfile('Username-data/data.txt')):
+    f = open('Username-data/data.txt', "r")
+    username = f.readline()
+    password = f.readline()
+else:
+    print("Sayonara")
+    exit()
+
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(1)
+
+driver.get("http://www.instagram.com")
+
+time.sleep(1)
+
+element = driver.find_element_by_link_text("Log in");
+element.click()
+
+time.sleep(1)
+
+to_insert_user = driver.find_element_by_name("username")
+to_insert_user.send_keys(username)
+to_insert_password = driver.find_element_by_name("password")
+to_insert_password.send_keys(password)
+
+time.sleep(1)
+
+to_insert_password.send_keys(Keys.RETURN)
+
+# driver.find_element_by_css_selector(".button_main").click()
+# element = driver.find_element_by_link_text("Log in");
+# element.click()
+
+
