b57463673dfdb1e59b0908a97bd951020b7240d7
ErikPillon <erik.pillon@gmail.com>
2019-03-15 20:47:33 +0100

    Added the possibility of sequentially like photos

--- Insta.py
+++ Insta.py
@@ -22,16 +22,20 @@ class Insta_bot:
             self.username = f.readline()
             self.password = f.readline()
         else:
+            f = open('Username-data/data.txt', "w")
             self.username = input("Please insert your username:")
+            f.write(self.username +'\n')
             print("Please insert your password:")
             self.password = getpass.getpass()
+            f.write(self.password)
+            
     
     # login through the browser
     def login(self):
         driver = self.driver
         driver.get("http://www.instagram.com")
         time.sleep(1)
-        element = driver.find_element_by_link_text("Log in");
+        element = driver.find_element_by_link_text("Log in")
         element.click()
         time.sleep(1)
         to_insert_user = driver.find_element_by_name("username")
@@ -45,45 +49,47 @@ class Insta_bot:
     def not_now(self):
         driver = self.driver
 #        driver.find_element_by_css_selector("aOOlW HoLwm ").click()
-        driver.find_element_by_xpath("//button[text()='Not Now']").click();    
-
-    def like_photo(self, hashtag):
-        driver = self.driver
-        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
-        
-        time.sleep(3)
-        for i in range(1,3):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(1)
-        
-        link_to_exploit = driver.find_elements_by_tag_name('a')
-        pic_hrefs = [elem.get_attribute('href') for elem in link_to_exploit]
-        
-        print(hashtag + " photo:"+str(len(pic_hrefs))) 
-        #for seeing how many photos are there 
-        # and now come the loop for visiting the page of the photos
-        click_number = 0
-        while click_number < 50:
-            for pic_href in pic_hrefs:
-                driver.get(pic_href)
-                time.sleep(5) # pause 5 sec after login in istagram
-                Heart= driver.find_element_by_css_selector('.coreSpriteHeartOpen')
-                Heart.click()     
-                click_number +=1
+        driver.find_element_by_xpath("//button[text()='Not Now']").click()    
 
     def collect_photo_to_like(self, hashtag):
-	driver = self.driver
+        driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         for i in range(1,3):
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(1)
-        # now I only collect the link for the images
+            # now I only collect the link for the images
         link_to_exploit = driver.find_elements_by_tag_name('a')
         pic_hrefs = [elem.get_attribute('href') for elem in link_to_exploit]
+        # pic_hrefs = [elem.find('https://www.instagram.com/p/') for elem in pic_hrefs]
         f = open('Username-data/to_like.txt', "a+")
         for href in pic_hrefs:
             if not href in f:
                 f.write(href+'\n')
+                # please note that not all the links are needed! 
+                # All the pictures have the form of https://www.instagram.com/p/something
+
+    def like_photo(self, link):
+        print(link)
+        if link.find('https://www.instagram.com/p/'):
+            driver = self.driver
+            print(link)
+            driver.get(link)
+            Heart= driver.find_element_by_css_selector('.coreSpriteHeartOpen')
+            Heart.click() 
+        else:
+            return
+    
+    def sequential_like_photo(self, max = 200):
+        to_wait = int(3600/max)
+        links_file = 'Username-data/to_like.txt'
+        if not (os.path.isfile(links_file)):
+            print('No picture to like found!')
+        else:
+            f = open(links_file, 'r')
+            for line in f:
+                self.like_photo(line)
+                time.sleep(to_wait)
+
 
 def main():
     Phi_Erik = Insta_bot()
@@ -91,7 +97,8 @@ def main():
     Phi_Erik.login()
 #    Phi_Erik.not_now()
     Phi_Erik.collect_photo_to_like('F1')
-    
+    Phi_Erik.sequential_like_photo()
+
 if __name__ == "__main__":
     main()
 
